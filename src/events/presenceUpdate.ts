d01678b47a660842acc257f07cfdf822f7def72f
Emigrek <kaioshinbox@gmail.com>
2022-12-30 14:38:47 +0100

    presence activity bugs

--- src/events/presenceUpdate.ts
+++ src/events/presenceUpdate.ts
@@ -6,18 +6,24 @@ export const presenceUpdate: Event = {
     name: "presenceUpdate",
     run: async (client, oldPresence, newPresence) => {
         const { guild, member } = newPresence;
-        const memberGuilds = client.guilds.cache.filter(g => g.members.cache.has(member.id));
+        const memberGuilds = client.guilds.cache.filter(g => g.members.cache.has(member.id)); 
 
-        if(guild.id != memberGuilds.first()!.id) return;
-        if(!oldPresence?.status || !newPresence?.status) return;
+        if(!guild.equals(memberGuilds.first()!)) return;
 
         const oldStatus = oldPresence?.status;
         const newStatus = newPresence?.status;
 
-        if(oldStatus === newStatus) return;
-        else if(oldStatus === PresenceUpdateStatus.Offline && newStatus !== PresenceUpdateStatus.Offline) {
+        if(
+            (oldStatus === PresenceUpdateStatus.Offline || oldStatus === PresenceUpdateStatus.Invisible)
+             && 
+            (newStatus !== PresenceUpdateStatus.Offline || newStatus !== PresenceUpdateStatus.Invisible)
+        ) {
             await startPresenceActivity(client, member, newPresence);
-        } else if(oldStatus !== PresenceUpdateStatus.Offline && newStatus === PresenceUpdateStatus.Offline) {
+        } else if(
+            (oldStatus !== PresenceUpdateStatus.Offline || oldStatus !== PresenceUpdateStatus.Invisible)
+             && 
+            (newStatus === PresenceUpdateStatus.Offline || newStatus === PresenceUpdateStatus.Invisible)
+        ) {
             await endPresenceActivity(client, member);
         }
     }
