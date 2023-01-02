2e186b192b18436a05d5734840f68268d80a3dec
Emigrek <kaioshinbox@gmail.com>
2023-01-02 23:08:39 +0100

    fixed level up notification

--- src/events/userLeveledUp.ts
+++ src/events/userLeveledUp.ts
@@ -1,25 +1,28 @@
 import { Guild, TextChannel } from "discord.js";
 import { Guild as DatabaseGuild } from "../interfaces";
 import { Event } from "../interfaces";
-import { everyGuild } from "../modules/guild";
+import { everyGuild, getGuilds } from "../modules/guild";
 import { getLevelUpMessagePayload } from "../modules/messages";
 import { assignUserLevelRole } from "../modules/roles";
 
 export const userLeveledUp: Event = {
     name: "userLeveledUp",
     run: async (client, user) => {
-        await everyGuild(client, async (guild: Guild, sourceGuild: DatabaseGuild) => {
+        const sourceGuilds = await getGuilds();
+
+        for await (const sourceGuild of sourceGuilds) {
+            const guild = client.guilds.cache.get(sourceGuild.id) as Guild;
             const { notifications, channelId, levelRoles } = sourceGuild;
-            if(!notifications || !channelId) return;
+            if(!notifications || !channelId) continue;
 
             const channel = guild.channels.cache.get(channelId) as TextChannel;
-            if(!channel) return;
+            if(!channel) continue;
             if(levelRoles) {
                 await assignUserLevelRole(client, user, guild);
             }
 
             const levelUpMesssagePayload = await getLevelUpMessagePayload(client, user, guild);
             await channel.send(levelUpMesssagePayload);
-        });
+        }
     }
 }
