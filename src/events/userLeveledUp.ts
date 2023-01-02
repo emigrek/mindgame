92109df7cd8237d38b8546faacbea7fc845aae04
Emigrek <kaioshinbox@gmail.com>
2023-01-02 23:09:38 +0100

    typo

--- src/events/userLeveledUp.ts
+++ src/events/userLeveledUp.ts
@@ -11,7 +11,7 @@ export const userLeveledUp: Event = {
         const sourceGuilds = await getGuilds();
 
         for await (const sourceGuild of sourceGuilds) {
-            const guild = client.guilds.cache.get(sourceGuild.id) as Guild;
+            const guild = client.guilds.cache.get(sourceGuild.guildId) as Guild;
             const { notifications, channelId, levelRoles } = sourceGuild;
             if(!notifications || !channelId) continue;
 
