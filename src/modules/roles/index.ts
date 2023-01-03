b50f2da76af427bc21f14c0d736b722fabdb8784
Emigrek <kaioshinbox@gmail.com>
2023-01-03 04:45:05 +0100

    reverted level roles changes

--- src/modules/roles/index.ts
+++ src/modules/roles/index.ts
@@ -1,20 +1,18 @@
-import { BitField, BitFieldResolvable, ColorResolvable, Guild, GuildMember, PermissionFlagsBits, Permissions, PermissionsBitField, Role, User } from "discord.js";
+import { ColorResolvable, Guild, GuildMember, Role, User } from "discord.js";
 import ExtendedClient from "../../client/ExtendedClient";
 import { Guild as DatabaseGuild, User as DatabaseUser } from "../../interfaces";
 import { getGuild, getGuilds } from "../guild";
 import { sendToDefaultChannel } from "../messages";
 import { getUser } from "../user";
-import Discord from "discord.js";
 
 interface LevelTreshold {
     level: number;
     color: ColorResolvable;
-    permissions: string[];
     position?: number;
 }
 
 const getLevelRoleTreshold = (level: number) => {
-    const tresholds = require("./tresholds.json") as LevelTreshold[];
+    const tresholds = require("./tresholds.json");
 
     let result = tresholds[0];
     tresholds.forEach((treshold: LevelTreshold) => {
@@ -25,31 +23,6 @@ const getLevelRoleTreshold = (level: number) => {
 
     let position = tresholds.indexOf(result)+1;
     return {...result, position};
-}
-
-const getLevelRoleTresholds = () => {
-    const tresholds = require("./tresholds.json") as LevelTreshold[];
-
-    tresholds.forEach((treshold: LevelTreshold) => {
-        let position = tresholds.indexOf(treshold)+1;
-        treshold.position = position;
-    });
-
-    return tresholds;
-}
-
-const checkGuildLevelRolesValid = async (guild: Guild) => {
-    const levelRoles = guild.roles.cache.filter(role => role.name.includes("Level"));
-    if(!levelRoles.size) return false;
-
-    const tresholds = getLevelRoleTresholds();
-    let valid = true;
-    tresholds.forEach((treshold: LevelTreshold) => {
-        const levelRole = levelRoles.find(role => role.name.includes("Level") && role.name.includes(treshold.level.toString()));
-        if(!levelRole) valid = false;
-    });
-
-    return valid;
 }
 
 const getGuildTresholdRole = (guild: Guild, treshold: LevelTreshold) => {
@@ -64,33 +37,11 @@ const getMemberTresholdRole = (member: GuildMember) => {
     return levelRole;
 }
 
-const addLevelRoles = async (client: ExtendedClient, guild: Guild) => {
-    const sourceGuild = await getGuild(guild) as DatabaseGuild;
-    const tresholds = getLevelRoleTresholds();
-
-    const levelRolesPromises = tresholds.map(async (treshold: LevelTreshold) => {
-        let tresholdRole = await guild.roles.create({
-            name: `Level ${treshold.level}`,
-            color: treshold.color,
-            hoist: sourceGuild.levelRolesHoist,
-            position: 0,
-        });
-        return tresholdRole;
-    });
-
-    const levelRoles = await Promise.all(levelRolesPromises);
-    return levelRoles;
-}
-
 const syncGuildLevelRoles = async (client: ExtendedClient, guild: Guild) => {
     const sourceGuild = await getGuild(guild) as DatabaseGuild;
     const levelRoles = guild.roles.cache.filter(role => role.name.includes("Level"));
 
     if(sourceGuild.levelRoles) {
-        if(!levelRoles.size) {
-            await addLevelRoles(client, guild);
-        }
-
         await assignLevelRolesInGuild(client, guild);
     } else {
         if(!levelRoles.size) return null;
@@ -123,9 +74,6 @@ const assignUserLevelRole = async (client: ExtendedClient, user: User, guild: Gu
     const sourceUser = await getUser(user) as DatabaseUser;
     if(!sourceUser) return null;
 
-    const validLevelRoles = await checkGuildLevelRolesValid(guild);
-    if(!validLevelRoles) return null;
-
     const sourceGuild = await getGuild(guild) as DatabaseGuild;
     const member = await guild.members.fetch(user); 
     const currentTresholdRole = await getMemberTresholdRole(member);
@@ -143,8 +91,22 @@ const assignUserLevelRole = async (client: ExtendedClient, user: User, guild: Gu
         }
     }
 
+    if(!tresholdRole) {
+        try {
+            tresholdRole = await guild.roles.create({
+                name: `Level ${treshold.level}`,
+                color: treshold.color,
+                hoist: sourceGuild.levelRolesHoist,
+                position: treshold.position
+            });
+        } catch (error) {
+            await sendToDefaultChannel(client, guild, client.i18n.__("roles.missingPermissions"));
+            return null;
+        }
+    }
+
     try {
-        await member.roles.add(tresholdRole!);
+        await member.roles.add(tresholdRole);
     } catch (error) {
         await sendToDefaultChannel(client, guild, client.i18n.__("roles.missingPermissions"));
         return null;
