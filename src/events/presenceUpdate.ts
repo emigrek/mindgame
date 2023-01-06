7be16d1edabc10cf5fbb327bb5332395da917839
Emigrek <kaioshinbox@gmail.com>
2023-01-06 21:56:03 +0100

    removed debug log

--- src/events/presenceUpdate.ts
+++ src/events/presenceUpdate.ts
@@ -5,7 +5,6 @@ import { endPresenceActivity, startPresenceActivity } from "../modules/activity"
 export const presenceUpdate: Event = {
     name: "presenceUpdate",
     run: async (client, oldPresence, newPresence) => {
-        console.log("presenceUpdate");
         const { guild, member } = newPresence;
 
         const oldStatus = oldPresence?.status;
