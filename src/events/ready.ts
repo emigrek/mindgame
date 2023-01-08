c1eaa6273b8d0283b563ebd428d1d6ca20049725
Emigrek <kaioshinbox@gmail.com>
2023-01-08 02:42:11 +0100

    removed debug lines

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -25,18 +25,8 @@ export const ready: Event = {
     run: async (client) => {
         console.log(`[ready] Logged in as`, client.user?.tag);
         console.log(`[ready] Serving`, client.guilds.cache.size, `guilds`);
+        
         await updatePresence(client);
-
-        const clientGuilds = client.guilds.cache;
-        for await (const guild of clientGuilds.values()) {
-            const g = await client.guilds.fetch({
-                guild: guild.id,
-                withCounts: false
-            });
-            await g.members.fetch({
-                withPresences: false
-            });
-        }
         //await restPutRes(client);
     }
 }
