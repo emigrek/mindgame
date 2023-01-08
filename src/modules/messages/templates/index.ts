ffa33d4535bb03ce9c790d19bea0d86b309ad4b9
Emigrek <kaioshinbox@gmail.com>
2023-01-08 07:22:55 +0100

    switched from opacity to saturating

--- src/modules/messages/templates/index.ts
+++ src/modules/messages/templates/index.ts
@@ -115,11 +115,13 @@ const getStatisticsTable = (guildStatistics: any, colors: ImageHexColors) => {
 
             let shadowColor = chromaColor.alpha(hoursAlpha/100).rgba().join(',');
             let hourAlpha = Math.round((hour.activePeak/dayStat.activePeak) * 100);
+            let hourHsl = chromaColor.hsl();
+            let hourColor = chroma.hsl(hourHsl[0], hourAlpha/100, hourHsl[2]).rgba().join(',');
 
             return `<td class="m-0 p-0 w-7 h-7" style="box-shadow: 0 0 10px rgba(${shadowColor});">
                 <div 
-                    class="text-center bg-[${colors.Vibrant}] w-7 h-7 text-black/60 font-bold text-sm flex items-center justify-center"
-                    style="opacity: ${hourAlpha}%;"
+                    class="text-center w-7 h-7 text-black/60 font-bold text-sm flex items-center justify-center"
+                    style="background-color: rgba(${hourColor});"
                 >
                     ${hour.activePeak}
                 </div>
