c72992ba33d84b7388ef0154a3cbde134300af49
Emigrek <kaioshinbox@gmail.com>
2023-01-09 06:34:11 +0100

    -restPutRes

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -29,6 +29,5 @@ export const ready: Event = {
         console.log(`[ready] Serving`, client.guilds.cache.size, `guilds`);
         
         await updatePresence(client);
-        await restPutRes(client);
     }
 }
