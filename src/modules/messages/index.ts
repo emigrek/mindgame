926587d2d3f3e0fb036062ffd33b8a6cbc237786
Emigrek <kaioshinbox@gmail.com>
2023-01-09 05:14:03 +0100

    hour formatting in level up notification

--- src/modules/messages/index.ts
+++ src/modules/messages/index.ts
@@ -174,7 +174,7 @@ const getLevelUpMessagePayload = async (client: ExtendedClient, user: User, guil
             },
             { 
                 name: client.i18n.__("notifications.weekVoiceTimeField"),
-                value: `\`\`\`${Math.round((sourceUser.week.time.voice)/3600)} H\`\`\``,
+                value: `\`\`\`${Math.round((sourceUser.week.time.voice)/3600)}H\`\`\``,
                 inline: true
             }
         ],
