79840636e9b98250100d8b808b076748bc506d65
Emigrek <kaioshinbox@gmail.com>
2023-01-14 06:42:06 +0100

    reverted guild activity statistics changes

--- src/modules/activity/index.ts
+++ src/modules/activity/index.ts
@@ -171,18 +171,21 @@ const mockDays = () => {
 };
 
 const getActiveUsersInHour = (activities: VoiceActivity[] | PresenceActivity[], day: number, hour: number): number => {
-    const hourMoment = moment().day(day).hour(hour);
-    const filtered = [...activities].filter(a => {
-        let from = moment(a.from);
-        let to = moment(a.to);
+    let activeUsers = new Set<string>();
+    for (const activity of activities) {
+        if(!activity.to) activity.to = moment().toDate();
+        
+        let from = moment(activity.from);
+        let to = moment(activity.to);
 
-        let dayCondition = from.isSame(hourMoment, 'day') && to.isSame(hourMoment, 'day');
-        let hourCondition = from.isSameOrBefore(hourMoment) || to.isSameOrAfter(hourMoment);
-
-        return dayCondition && hourCondition;
-    });
-    
-    return filtered.length;
+        if(
+            from.get('day') === day && (from.get('hours') <= hour || to.get('hours') >= hour) ||
+            to.get('day') === day && (from.get('hours') <= hour || to.get('hours') >= hour)
+        ) {
+            activeUsers.add(activity.userId);
+        }
+    }
+    return activeUsers.size;
 }
 
 const getActiveUsersInDay = (activities: VoiceActivity[] | PresenceActivity[], day: number): number => {
