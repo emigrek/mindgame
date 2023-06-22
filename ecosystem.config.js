edc4a222cc176dfb0015525c7fc36d13e91416f8
emigrek <jarasz.karol@gmail.com>
2023-06-22 17:16:23 +0200

    add pm2 timestamps and automatic restart at 4am

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  apps : [{
-    name: "MG",
-    script: "npm run start"
-  }]
+  apps: [
+    {
+      name: "MG",
+      script: "npm run start",
+      cron_restart: "0 4 * * *",
+      time: true
+    }
+  ]
 }
