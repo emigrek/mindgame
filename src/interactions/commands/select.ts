c487fabec1cd6859f30c2b0fecaec650d21d079e
emigrek <jarasz.karol@gmail.com>
2023-08-11 04:42:25 +0200

    initialize SelectOptionsStore on modal run to enable rerunning command without keeping the state

--- src/interactions/commands/select.ts
+++ src/interactions/commands/select.ts
@@ -2,12 +2,14 @@ import { Command } from "@/interfaces";
 import { SlashCommandBuilder } from "@discordjs/builders";
 import i18n from "@/client/i18n";
 import { getSelectOptionsModal } from "@/modules/messages/modals";
+import { selectOptionsStore } from "@/stores/selectOptionsStore";
 
 export const select: Command = {
     data: new SlashCommandBuilder()
         .setName("select")
         .setDescription(i18n.__("commandLocalizations.select.description")),
     execute: async (client, interaction) => {
+        selectOptionsStore.init(interaction.user.id);
         const selectModal = getSelectOptionsModal();
         await interaction.showModal(selectModal);
     }
