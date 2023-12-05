7dd443fde5ca36b485b531ddc3e76be57c56abed
emigrek <jarasz.karol@gmail.com>
2023-12-05 02:04:35 +0100

    Add delay before sweeping channel after guild empty

--- src/events/guildVoiceEmpty.ts
+++ src/events/guildVoiceEmpty.ts
@@ -3,12 +3,15 @@ import { Event } from "@/interfaces";
 import { getGuild } from "@/modules/guild";
 import { sweepTextChannel } from "@/modules/messages";
 import { Guild, VoiceChannel } from "discord.js";
+import { delay } from '@/utils/delay';
 
 export const guildVoiceEmpty: Event = {
     name: "guildVoiceEmpty",
     run: async (client: ExtendedClient, guild: Guild, lastChannel: VoiceChannel) => {
         const sourceGuild = await getGuild(guild);
         if (!sourceGuild || !sourceGuild.autoSweeping) return;
+
+        await delay(10 * 1000);
         
         await sweepTextChannel(client, lastChannel);
 
