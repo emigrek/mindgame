847db0c14458cebcdc3fb23f77196344bedeb5bd
emigrek <jarasz.karol@gmail.com>
2024-02-22 19:45:47 +0100

    Updated exp rates

--- src/modules/experience/expCalculatorConfig.ts
+++ src/modules/experience/expCalculatorConfig.ts
@@ -1,6 +1,6 @@
 import { ExpCalculatorConfig } from ".";
 
 export const expCalculatorConfig: ExpCalculatorConfig = {
-    base: 0.01,
-    voiceBase: 0.04
+    base: 0.01 / 60,
+    voiceBase: 0.04 / 60
 };
