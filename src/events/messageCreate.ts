b68248057bb9fa1aa2d9c0a10513f54f433f1159
emigrek <jarasz.karol@gmail.com>
2024-03-08 20:05:42 +0100

    Added little delay to avoid duplicating quick buttons component rows

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -4,17 +4,19 @@ import { attachQuickButtons } from "@/modules/messages"
 import { getEphemeralChannel, isMessageCacheable } from "@/modules/ephemeral-channel";
 import { ephemeralChannelMessageCache } from "@/modules/ephemeral-channel/cache";
 import ExtendedClient from "@/client/ExtendedClient";
+import { delay } from "@/utils/delay";
 
 export const messageCreate: Event = {
     name: "messageCreate",
     run: async (client: ExtendedClient, message: Message) => {
-        if(message.author.id === client.user?.id)
-            await attachQuickButtons(client, message.channel as TextChannel);
-
         const ephemeralChannel = await getEphemeralChannel(message.channel.id);
         if(ephemeralChannel) {
             const cachable = await isMessageCacheable(message);
             cachable && ephemeralChannelMessageCache.add(message.channel.id, message);
+        }
+
+        if(message.author.id === client.user?.id) {
+            delay(500).then(() => attachQuickButtons(client, message.channel as TextChannel));
         }
     }
 }
