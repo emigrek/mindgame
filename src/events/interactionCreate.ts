13e63ae21651c2a935c6115653847db989be6662
emigrek <jarasz.karol@gmail.com>
2024-03-19 21:52:17 +0100

    interactionCreate error catch

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,16 +1,15 @@
 import ExtendedClient from "@/client/ExtendedClient";
 import i18n from "@/client/i18n";
+import { keys } from "@/config";
 import { Event } from "@/interfaces";
 import { WarningEmbed } from "@/modules/messages/embeds";
 import { getUser, updateUserStatistics } from "@/modules/user";
-import { keys } from "@/config";
 import { BaseInteraction } from "discord.js";
 
 export const interactionCreate: Event = {
     name: "interactionCreate",
     run: async (client: ExtendedClient, interaction: BaseInteraction) => {
         i18n.setLocale(interaction.locale);
-
         if (interaction.isChatInputCommand()) {
             const command = client.commands.get(interaction.commandName);
             if (!command)
@@ -54,23 +53,32 @@ export const interactionCreate: Event = {
             }
 
             command.execute(client, interaction)
+                .catch((e) => console.log(`Error executing ChatInputCommand: ${e}`))
                 .then(() => {
                     updateUserStatistics(client, interaction.user, {
                         commands: 1
                     });
-                })
-        }
-        if (interaction.isModalSubmit()) {
-            client.modals.get(interaction.customId)?.run(client, interaction);
-        }
-        if (interaction.isAnySelectMenu()) {
-            client.selects.get(interaction.customId)?.run(client, interaction);
-        }
-        if (interaction.isButton()) {
-            client.buttons.get(interaction.customId)?.run(client, interaction);
-        }
-        if (interaction.isContextMenuCommand() || interaction.isUserContextMenuCommand() || interaction.isMessageContextMenuCommand()) {
-            client.contexts.get(interaction.commandName)?.run(client, interaction);
+                });
+        } else if (interaction.isModalSubmit()) {
+            client.modals
+                .get(interaction.customId)
+                ?.run(client, interaction)
+                .catch((e) => console.log(`Error executing ModalSubmit: ${e}`));
+        } else if (interaction.isAnySelectMenu()) {
+            client.selects
+                .get(interaction.customId)
+                ?.run(client, interaction)
+                .catch((e) => console.log(`Error executing SelectMenu: ${e}`));
+        } else if (interaction.isButton()) {
+            client.buttons
+                .get(interaction.customId)
+                ?.run(client, interaction)
+                .catch((e) => console.log(`Error executing Button: ${e}`));
+        } else if (interaction.isContextMenuCommand()) {
+            client.contexts
+                .get(interaction.commandName)
+                ?.run(client, interaction)
+                .catch((e) => console.log(`Error executing ContextMenuCommand: ${e}`));
         }
     }
-}
+}   
