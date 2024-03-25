29fdd7fc50c0ba66a3fd46a837741ca2fc54fcde
emigrek <jarasz.karol@gmail.com>
2024-03-25 02:57:57 +0100

    Migrate user model

--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,30 +1,6 @@
-export interface Statistics {
-  exp: number;
-  time: {
-    public: boolean;
-    voice: number;
-    presence: number;
-  };
-  games: {
-    won: {
-      skill: number;
-      skin: number; 
-    }
-  }
-}
-
-export interface ExtendedStatistics extends Statistics {
-  level: number;
-  commands: number;
-}
-
 export interface User {
   userId: string;
   username: string;
   avatarUrl: string;
-  stats: ExtendedStatistics;
-  day: Statistics,
-  week: Statistics,
-  month: Statistics,
   publicTimeStatistics: boolean;
 }
