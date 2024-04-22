1ae708df2d2d2596de2b69a3b48eacf417b39e42
emigrek <jarasz.karol@gmail.com>
2024-04-22 16:16:59 +0200

    Immediately removing voice and presence activities after guildMemberRemove

--- src/events/guildMemberRemove.ts
+++ src/events/guildMemberRemove.ts
@@ -1,23 +1,22 @@
 import ExtendedClient from "@/client/ExtendedClient";
-import { Event } from "@/interfaces";
-import { getMemberColorRole } from "@/modules/roles";
-import { deleteUserGuildStatistics } from "@/modules/user-guild-statistics";
-import { GuildMember } from "discord.js";
+import {Event} from "@/interfaces";
+import {getMemberColorRole} from "@/modules/roles";
+import {deleteUserGuildStatistics} from "@/modules/user-guild-statistics";
+import {GuildMember} from "discord.js";
+import {endPresenceActivity, endVoiceActivity} from "@/modules/activity";
 
 export const guildMemberRemove: Event = {
     name: "guildMemberRemove",
     run: async (client: ExtendedClient, member: GuildMember) => {
+        await endVoiceActivity(member);
+        await endPresenceActivity(member.user.id, member.guild.id);
         await deleteUserGuildStatistics({ 
             userId: member.id,
             guildId: member.guild.id
         });
-        const colorRole = getMemberColorRole(member);
-
-        if (colorRole) {
-            await colorRole.delete()
-                .catch(e => {
-                    console.log(`There was an error when removing level role after member left guild: ${e}`)
-                });
-        }
+        await getMemberColorRole(member)?.delete()
+            .catch(e => {
+                console.log(`There was an error when removing level role after member left guild: ${e}`)
+            });
     }
 }
